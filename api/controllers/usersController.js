71008cd44166c6715f1f9dc5cde0d1aee5812801
“Francsico239” <Jose.francisco_2000@outlook.com>
2022-07-19 21:59:08 -0500

    try catch

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -5,20 +5,25 @@ import  UserModel from '../models/User.js';
 const userController = {};
 
 userController.createUser =  (async (req, res) => {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const newUser = {
-        name: req.body.name,
-        lastName: req.body.lastName,
-        birthday: new Date(),
-        idNum: req.body.idNum,
-        phone: req.body.phone,
-        email: req.body.email,
-        residence: req.body.residence,
-        password: hashedPassword,
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = {
+            name: req.body.name,
+            lastName: req.body.lastName,
+            birthday: new Date(),
+            idNum: req.body.idNum,
+            phone: req.body.phone,
+            email: req.body.email,
+            residence: req.body.residence,
+            password: hashedPassword,
+        }
+        const createdUser = await UserModel.create(newUser);
+        res.status(201).json(createdUser)     
     }
-   
-    const createdUser = await UserModel.create(newUser);
-    res.status(201).json(createdUser)
+    catch (err) {
+        return res.status(500).json({err})
+    }
 })
 
 export default userController;
+   
