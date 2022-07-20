fc7b45a41b603411801cf275b7fd2a055758f43d
Daniel Isai <daisai1228@hotmail.com>
2022-07-19 21:54:37 -0500

    feat: add roles

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  lastName: String,
+  birthday: Date,
+  idNum: {
+    type: Number,
+    required: [true, 'Id number necessary'],
+  },
+  phone: Number,
+  email: {
+    type: String,
+    required: [true, 'Mail necessary'],
+  },
+  residence: String,
+  password: {
+    type: String,
+    required: [true, 'Password required'],
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+    required: [true, 'Role required'] 
+  }
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel;
