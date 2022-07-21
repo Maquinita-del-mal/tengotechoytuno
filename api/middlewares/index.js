cea570a6ee89d4b05054b76e2c0dd84c562b6930
servo pcerda <fernandonando98@live.com.mx>
2022-07-20 21:21:01 -0500

    fix: update validator

--- api/middlewares/index.js
+++ api/middlewares/index.js
@@ -1,4 +1,5 @@
 export { default as createUserValidator } from './createUserValideitor.js';
 export { default as loginUserValidator } from './loginUserValidator.js';
 export { default as protectedRoute } from './protectedMiddleware.js';
-export { default as authValidator } from './authValidator.js'
+export { default as authValidator } from './authValidator.js';
+export { default as updateUserValidator } from './updateUserVlidator.js';
