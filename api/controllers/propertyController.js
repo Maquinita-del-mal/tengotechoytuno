9fc8c67afd37630401b1276abb63167c93a0d297
Luis Lechuga <luislechu55@gmail.com>
2022-07-20 21:42:44 -0500

    feat: filters propertys

--- api/controllers/propertyController.js
+++ api/controllers/propertyController.js
@@ -48,4 +48,35 @@ const updateById = async (req, res) => {};
 
 const deleteById = async (req, res) => {};
 
-export { create, getAll, getById, updateById, deleteById };
+/**
+ * Filtro por rango de precio
+ * Filtro por país y/o localidad
+ * Filtro por tipo de inmueble
+ * Filtro por número de recámaras
+ * los filtros se pasan por medio de los req.query params
+ */
+
+const getFiltersPropertys = async (req, res) => {
+  const filters = req.query;
+  try {
+    const propertys = await Property.find(filters);
+    return res.json({
+      msg: 'Inmuebles obtenidos',
+      propertys,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      msg: 'Error al buscar los inmuebles',
+      error,
+    });
+  }
+};
+
+export {
+  create,
+  getAll,
+  getById,
+  updateById,
+  deleteById,
+  getFiltersPropertys,
+};
